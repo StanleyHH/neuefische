2c19d9f31b93733b3e042f8217dd36c2a93b6332
Stanislav Dering <stanislavdering@gmail.com>
2025-10-08 16:52:57 +0200

    Aufgabe 2: Rechnen und Vergleichen

---
+++ src/javabasics/datatypesexercises/tough/aufgabe02/Main.java
@@ -0,0 +1,15 @@
+package javabasics.datatypesexercises.tough.aufgabe02;
+
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 4.44;
+        double num2 = 44.22;
+        double sum = num1 + num2;
+
+        if (sum > 10) {
+            System.out.println("Die Summe ist größer als 10");
+        } else {
+            System.out.println("Die Summe ist kleiner oder gleich 10");
+        }
+    }
+}
