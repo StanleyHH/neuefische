7292c0d08480f9266eb3920c5f31d93460f7be66
Stanislav Dering <stanislavdering@gmail.com>
2025-10-09 11:41:17 +0200

    Methoden Exercise: bonus

---
+++ src/javabasics/methodenexercises/bonus/aufgabe01/Main.java
@@ -0,0 +1,11 @@
+package javabasics.methodenexercises.bonus.aufgabe01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isEven(3));
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+}
