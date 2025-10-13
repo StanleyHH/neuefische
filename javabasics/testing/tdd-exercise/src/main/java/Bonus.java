195cf78d7e57e2274234f612dfce64888fcfecc7
Stanislav Dering <stanislavdering@gmail.com>
2025-10-13 11:16:36 +0200

    create fibonacci method

--- javabasics/testing/tdd-exercise/src/main/java/Bonus.java
+++ javabasics/testing/tdd-exercise/src/main/java/Bonus.java
@@ -12,4 +12,18 @@ public class Bonus {
 
         return  result;
     }
+
+    public static int fibonacci(int n) {
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+
+        for (int i = 0; i < n; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+        return n1;
+    }
 }
