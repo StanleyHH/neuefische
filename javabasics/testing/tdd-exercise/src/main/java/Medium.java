01699e25193b5ac1fcb535780cdf8053cad2399a
Stanislav Dering <stanislavdering@gmail.com>
2025-10-13 11:02:39 +0200

    create countVowels method

--- javabasics/testing/tdd-exercise/src/main/java/Medium.java
+++ javabasics/testing/tdd-exercise/src/main/java/Medium.java
@@ -10,4 +10,17 @@ public class Medium {
     public static boolean isPalindrome(String text) {
         return text.equalsIgnoreCase(reverseString(text));
     }
+
+    public static int countVowels(String text) {
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
