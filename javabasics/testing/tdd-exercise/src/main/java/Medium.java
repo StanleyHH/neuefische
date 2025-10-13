e8dd7a125b606efc384c6e1e2e45f16b3671532b
Stanislav Dering <stanislavdering@gmail.com>
2025-10-13 10:48:50 +0200

    create reverseString method

---
+++ javabasics/testing/tdd-exercise/src/main/java/Medium.java
@@ -0,0 +1,9 @@
+public class Medium {
+    public static void main(String[] args) {
+
+    }
+
+    public static String reverseString(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+}
