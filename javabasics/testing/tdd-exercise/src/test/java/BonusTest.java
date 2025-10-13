030043f48f3373274d5206c9614cd9511c08bd45
Stanislav Dering <stanislavdering@gmail.com>
2025-10-13 11:05:09 +0200

    create factorial test

---
+++ javabasics/testing/tdd-exercise/src/test/java/BonusTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusTest {
+
+    @Test
+    void factorial_ShouldReturn120_When5IsGiven() {
+        int expected = 120;
+        assertEquals(expected, Bonus.factorial(5));
+    }
+}
