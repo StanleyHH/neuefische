0e6598559a310641470bf43a3ba8336911594ffa
Stanislav Dering <stanislavdering@gmail.com>
2025-10-13 11:06:45 +0200

    create factorial method

---
+++ javabasics/testing/tdd-exercise/src/main/java/Bonus.java
@@ -0,0 +1,15 @@
+public class Bonus {
+    public static void main(String[] args) {
+
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return  result;
+    }
+}
